12b058860c6f853f76ff0130f009022c23d0a7a6
gabrieltherock <gabrieldarocha2013@hotmail.com>
2022-03-13 17:46:24 -0300

    aplicando o padrão State para gerenciar casos em que o desconto depende de um estado específico do orçamento

--- loja/src/main/java/br/com/gabriel/orcamento/Orcamento.java
+++ loja/src/main/java/br/com/gabriel/orcamento/Orcamento.java
@@ -4,12 +4,15 @@ import java.math.BigDecimal;
 
 public class Orcamento {
 
-    private final BigDecimal valor;
+    private BigDecimal valor;
     private final int quantidadeItens;
+
+    private AbstractSituacaoOrcamento situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+        this.situacao = new EmAnalise();
     }
 
     public int getQuantidadeItens() {
@@ -18,5 +21,30 @@ public class Orcamento {
 
     public BigDecimal getValor() {
         return valor;
+    }
+
+    public void aplicarDescontoExtra() {
+        BigDecimal valorDescontoExtra = situacao.calcularValorDescontoExtra(this);
+        this.valor = this.valor.subtract(valorDescontoExtra);
+    }
+
+    public void aprovar() {
+        this.situacao.aprovar(this);
+    }
+
+    public void reprovar() {
+        this.situacao.reprovar(this);
+    }
+
+    public void finalizar() {
+        this.situacao.finalizar(this);
+    }
+
+    public AbstractSituacaoOrcamento getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(AbstractSituacaoOrcamento situacao) {
+        this.situacao = situacao;
     }
 }
