2440e03fd13bf48a220ddbba4ef09c3474a3f6f9
gabrieltherock <gabrieldarocha2013@hotmail.com>
2022-03-20 12:02:51 -0300

    padrão Observer com classes que reagem a eventos

--- loja/src/main/java/br/com/gabriel/TestesPedidos.java
+++ loja/src/main/java/br/com/gabriel/TestesPedidos.java
@@ -2,19 +2,22 @@ package br.com.gabriel;
 
 import br.com.gabriel.pedido.GeraPedido;
 import br.com.gabriel.pedido.GeraPedidoHandler;
+import br.com.gabriel.pedido.acao.EnviarEmailPedido;
+import br.com.gabriel.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 public class TestesPedidos {
 
     public static void main(String[] args) {
-        String cliente = args[0];
-        BigDecimal valorOrcamento = new BigDecimal(args[1]);
-        int quantidadeItens = Integer.parseInt(args[2]);
+        String cliente = "Gabriel";
+        BigDecimal valorOrcamento = new BigDecimal("300");
+        int quantidadeItens = 2;
 
         GeraPedido geraPedido = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
 
-        GeraPedidoHandler handler = new GeraPedidoHandler();
+        GeraPedidoHandler handler = new GeraPedidoHandler(Arrays.asList(new EnviarEmailPedido(), new SalvarPedidoNoBancoDeDados()));
         handler.execute(geraPedido);
     }
 }
--- loja/src/main/java/br/com/gabriel/pedido/GeraPedidoHandler.java
+++ loja/src/main/java/br/com/gabriel/pedido/GeraPedidoHandler.java
@@ -1,16 +1,25 @@
 package br.com.gabriel.pedido;
 
 import br.com.gabriel.orcamento.Orcamento;
+import br.com.gabriel.pedido.acao.AcaoAposGerarPedido;
+import br.com.gabriel.pedido.acao.EnviarEmailPedido;
+import br.com.gabriel.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class GeraPedidoHandler {
+
+    List<AcaoAposGerarPedido> acoes;
+
+    public GeraPedidoHandler(List<AcaoAposGerarPedido> acoes) {
+        this.acoes = acoes;
+    }
 
     public void execute(GeraPedido dados) {
         Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
         Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
 
-        System.out.println("Salvar pedido no banco de dados");
-        System.out.println("Enviar email com dados do novo pedido");
+        acoes.forEach(acao -> acao.executarAcao(pedido));
     }
 }
